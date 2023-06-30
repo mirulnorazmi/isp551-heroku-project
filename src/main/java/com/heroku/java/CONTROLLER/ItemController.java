fbe8e366df44fe72203310651d348b4439ae01c1
AhmadZaidMaarof <2021843412@student.uitm.edu.my>
2023-06-30 22:17:23 +0800

    backend for stuff

--- src/main/java/com/heroku/java/CONTROLLER/ItemController.java
+++ src/main/java/com/heroku/java/CONTROLLER/ItemController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import java.sql.Date;
 import com.heroku.java.MODEL.ItemsDry;
+import com.heroku.java.MODEL.ItemsStuff;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -42,13 +43,13 @@ public class ItemController {
 
   @GetMapping("/create-items/create-item-stuff")
   public String stuff(HttpSession session) {
-    // if (session.getAttribute("username") != null) {
-    // return "supervisor/PAGE_CREATE_ITEM/create-item-wet";
-    // } else {
-    // System.out.println("No valid session or session...");
-    // return "redirect:/";
-    // }
+    if (session.getAttribute("username") != null) {
     return "supervisor/PAGE_CREATE_ITEM/create-item-stuff";
+    } else {
+    System.out.println("No valid session or session...");
+    return "redirect:/";
+    }
+    // return "supervisor/PAGE_CREATE_ITEM/create-item-stuff";
   }
 
   @GetMapping("/create-items/create-item-wet")
@@ -106,4 +107,93 @@ public class ItemController {
       return "redirect:/accounts/create-account?success=false";
     }
   }
+
+  @PostMapping("/create-item-stuff")
+  public String createItemDry(@ModelAttribute("createStuff") ItemsStuff stuff, HttpSession session) {
+    try {
+      if (stuff.getName().equals(null) && stuff.getQuantity() == 0 && stuff.getAdded_date().equals(null)) {
+        return "redirect:/create-items/create-item-stuff?success=false";
+      } else {
+        Connection connection = dataSource.getConnection();
+        String sql_items = "INSERT INTO items(name, quantity, added_date) VALUES (?,?,?)RETURNING itemsid AS itemsid;";
+        final var pstatement1 = connection.prepareStatement(sql_items);
+        pstatement1.setString(1, stuff.getName());
+        pstatement1.setInt(2, stuff.getQuantity());
+        pstatement1.setDate(3, stuff.getAdded_date());
+        pstatement1.execute();
+        ResultSet items_d = pstatement1.getResultSet();
+        int items_id = 0;
+        if (items_d.next()) {
+          items_id = items_d.getInt(1);
+        }
+
+        System.out.println(">>>>Item [" + items_id + "] created by staff[" + session.getAttribute("staffid") + "] " + session.getAttribute("username"));
+        String sql_stuff = "INSERT INTO furniture(itemsid, location, warranty) VALUES (?,?,?)";
+
+        final var pstatement2 = connection.prepareStatement(sql_stuff);
+        pstatement2.setInt(1, items_id);
+        pstatement2.setString(2, stuff.getlocation());
+        pstatement2.setString(3, stuff.getWarranty());
+        pstatement2.executeUpdate();
+
+        return "redirect:/create-items/create-item-stuff?success=true";
+      }
+
+    } catch (SQLException sqe) {
+      System.out.println("Error Code = " + sqe.getErrorCode());
+      System.out.println("SQL state = " + sqe.getSQLState());
+      System.out.println("Message = " + sqe.getMessage());
+      System.out.println("printTrace /n");
+      sqe.printStackTrace();
+
+      return "redirect:/";
+    } catch (Exception e) {
+      System.out.println("E message : " + e.getMessage());
+      return "redirect:/";
+    }
+  }
+
+
+//  @PostMapping("/create-item-stuff")
+//   public String itemsStuff(HttpSession session, @ModelAttribute("createStuff") ItemsStuff stuff) {
+//         try {
+//             Connection connection = dataSource.getConnection();
+//             String sql = "INSERT INTO item_stuff(name, quantity, date, allocation, warranty) VALUES (?,?,?,?,?)";
+//             final var statement = connection.prepareStatement(sql);
+
+//             statement.setString(1, stuff.getName());
+//             statement.setInt(2, stuff.getQuantity());
+//             statement.setDate(3, stuff.getDate());
+//             statement.setString(4, stuff.getAllocation());
+//             statement.setString(5, stuff.getWarranty());
+//             statement.executeUpdate();
+
+//             // debug
+//             System.out.println("name" + stuff.getName());
+//             System.out.println("quantity" + stuff.getQuantity());
+//             System.out.println("date" +stuff.getDate());
+//             System.out.println("allocation" + stuff.getAllocation());
+//             System.out.println("warranty" +stuff.getWarranty());
+
+//             connection.close();
+
+//             return "redirect:/create-items/create-item-stuff";
+
+//         }
+
+//         catch (SQLException sqe) {
+//             System.out.println("Error Code = " + sqe.getErrorCode());
+//             System.out.println("SQL state = " + sqe.getSQLState());
+//             System.out.println("Message = " + sqe.getMessage());
+//             System.out.println("printTrace /n");
+//             sqe.printStackTrace();
+
+//             return "redirect:/";
+//         } catch (Exception e) {
+//             System.out.println("E message : " + e.getMessage());
+//             return "redirect:/";
+//         }
+
+//     }
+
 }
---
+++ src/main/java/com/heroku/java/MODEL/ItemsStuff.java
@@ -0,0 +1,26 @@
+package com.heroku.java.MODEL;
+
+
+public class ItemsStuff extends Items {
+   
+
+  private String location;
+  private String warranty;
+  
+
+public String getlocation() {
+    return location;
+}
+public void setAllocation(String allocation) {
+    this.location = allocation;
+}
+public String getWarranty() {
+    return warranty;
+}
+public void setWarranty(String warranty) {
+    this.warranty = warranty;
+}
+
+  
+
+}
