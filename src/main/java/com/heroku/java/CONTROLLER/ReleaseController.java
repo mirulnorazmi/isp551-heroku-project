cb81fe0d221b8cd6fbf1c8cfd7ef26d984773c04
AmirulDaniel <2021888302@student.uitm.edu.my>
2023-07-16 00:52:29 +0800

    added release backend

--- src/main/java/com/heroku/java/CONTROLLER/ReleaseController.java
+++ src/main/java/com/heroku/java/CONTROLLER/ReleaseController.java
@@ -3,11 +3,17 @@ package com.heroku.java.CONTROLLER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.heroku.java.MODEL.Items;
+import com.heroku.java.SERVICES.ItemServices;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.apache.logging.log4j.*;
 
@@ -16,20 +22,39 @@ import javax.sql.DataSource;
 
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class ReleaseController {
+  private ItemServices itemServices;
+
+  @Autowired
+  public ReleaseController(ItemServices itemServices) {
+    this.itemServices = itemServices;
+  }
 
   @GetMapping("/release-item")
-  public String release(HttpSession session) {
-    // if (session.getAttribute("username") != null) {
-    //   return "redirect:/dashboard";
-    // } else {
-    //   System.out.println("No valid session or session...");
-    //   return "/supervisor/PAGE_ACCOUNT/create-account";
-    // }
-    return "/supervisor/PAGE_RELEASE_ITEM/release-item";
+  public String release(HttpSession session,
+      @RequestParam(value = "create_success", defaultValue = "false") boolean createSuccess, Model model)
+      throws Exception {
+    if (session.getAttribute("username") != null) {
+      ArrayList<Items> itemList = itemServices.getAllItems();
+      model.addAttribute("items", itemList);
+      return "supervisor/PAGE_RELEASE_ITEM/release-item";
+    } else {
+      System.out.println("No valid session or session...");
+      return "redirect:/";
+    }
   }
- 
+
+  @PostMapping("/release-item/confirm")
+  public String confirmRelease(@RequestParam(name = "itemsid") int[] itemIds,
+  @RequestParam(name = "itemquantity") int[] itemQuantities,
+   Model model) {
+    for (int i = 0; i < itemIds.length; i++) {
+      System.out.println("ID: " + itemIds[i] + " Quantity: " + itemQuantities[i]);
+  }
+    return "redirect:/release-item";
+  }
 }
