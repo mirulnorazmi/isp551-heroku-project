bd4e1cd52d3208a9c55fc459c711e22980a8ac7e
mirulnorazmi <mmiirul00@gmail.com>
2023-07-17 06:19:38 +0800

    generate item report

---
+++ src/main/java/com/heroku/java/HELPER/PdfGenerator.java
@@ -0,0 +1,23 @@
+package com.heroku.java.HELPER;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+
+import com.heroku.java.MODEL.Items;
+// import com.example.entity.Student;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.CMYKColor;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class PdfGenerator {
+
+}
